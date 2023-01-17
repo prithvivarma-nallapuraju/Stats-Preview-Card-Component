cd88a647e6aa55542cdaa5d38fff67fca6b0f015
Prithvi Varma Nallapuraju <98509310+prithvivarma-nallapuraju@users.noreply.github.com>
2023-01-17 09:44:36 +0530

    added custom styles and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "375px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        veryDarkBlue: "hsl(233, 47%, 7%)",
+        desaturatedBlue: "hsl(244, 38%, 16%)",
+        softViolet: "hsl(277, 64%, 61%)",
+        slightlyTransparentWhite: "hsla(0, 0%, 100%, 0.75)",
+        tranparentWhite: "hsla(0, 0%, 100%, 0.6)",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        lexendDeca: ["Lexend Deca", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
